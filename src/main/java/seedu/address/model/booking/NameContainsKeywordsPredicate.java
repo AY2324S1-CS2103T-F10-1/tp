8dc24596c561c5e9b88707189ed4ef4e21854f64
JiHoon <healingdog2@gmail.com>
2023-10-11 21:34:40 +0800

    Update FIndCommand to support search by room number

--- src/main/java/seedu/address/model/booking/NameContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/booking/NameContainsKeywordsPredicate.java
@@ -19,7 +19,10 @@ public class NameContainsKeywordsPredicate implements Predicate<Booking> {
     @Override
     public boolean test(Booking booking) {
         return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(booking.getName().fullName, keyword));
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(booking.getName().fullName, keyword))
+                || keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(Integer.toString(booking.getRoom().value),
+                        keyword));
     }
 
     @Override
